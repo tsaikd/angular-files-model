e9dfaba04d06940046c6a11b20f098911eee4ff1
tsaikd <tsaikd@gmail.com>
2014-03-14 12:47:59 +0800

    v0.1.0

---
+++ angular-files-model.js
@@ -0,0 +1,29 @@
+var app = angular
+
+.module("angular-files-model", [])
+
+;
+app
+
+.directive("ngFilesModel", ["$parse", function($parse) {
+	return {
+		restrict: "A",
+		link: function(scope, elem, attrs) {
+			elem.bind("change", function(evt) {
+				scope.$apply(function() {
+					var ngModelGet = $parse(attrs["ngFilesModel"]);
+					var ngModelSet = ngModelGet.assign;
+					ngModelSet(scope, evt.target.files);
+				});
+			});
+			scope.$watch(attrs["ngFilesModel"], function(file) {
+				// File input element accepts a filename, which may only be programatically set to the empty string.
+				if (!file) {
+					elem.val("");
+				}
+			});
+		}
+	};
+}])
+
+;
